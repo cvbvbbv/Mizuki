472929bac59cbca76c47caec918bf39f705c2242
cvbvbbv <132454113+cvbvbbv@users.noreply.github.com>
2025-12-06 10:39:50 +0800

    Add files via upload

--- src/data/devices.ts
+++ src/data/devices.ts
@@ -18,21 +18,29 @@ export type DeviceCategory = {
 export const devicesData: DeviceCategory = {
 	OnePlus: [
 		{
-			name: "OnePlus 13T",
-			image: "/images/device/oneplus13t.png",
-			specs: "Gray / 16G + 1TB",
-			description: "Flagship performance, Hasselblad imaging, 80W SuperVOOC.",
-			link: "https://www.oneplus.com/cn/13t",
+			name: "OnePlus ACE 2 PRO",
+			image: "/images/device/oneplus ace 2 pro.png",
+			specs: "Green/16G +512GB",
+			description: "Reconstructing performance imagination.",
+			link: "https://www.oneplus.com/cn/ace-2-pro",
 		},
 	],
-	Router: [
+	MiniPC: [
 		{
-			name: "GL-MT3000",
-			image: "/images/device/mt3000.png",
-			specs: "1000Mbps / 2.5G",
-			description:
-				"Portable WiFi 6 router suitable for business trips and home use.",
-			link: "https://www.gl-inet.cn/products/gl-mt3000/",
+			name: "AOOSTAR GEM 12 MAX",
+			image: "/images/device/AOOSTAR GEM12 MAX.png",
+			specs: "Black/R7-8745HS/32GB/1TB",
+			description: "Powerful Performance - Mini But Mighty!",
+			link: "https://www.sztbkj.com/minipc/pc-16",
+		},
+	],
+	Laptop: [
+		{
+			name: "ASUS TX GAMING FA608PM",
+			image: "/images/device/ASUS TX GAMING FA608PM.png",
+			specs: "Green/R9-8940HX/RTX 5060 Laptop/16GB/2TB",
+			description: "No Game No Life.",
+			link: "https://www.asus.com.cn/laptops/for-gaming/tuf-gaming/asus-tuf-gaming-a16-2025/",
 		},
 	],
 };
